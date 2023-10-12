2835e3fa4381129d7a58d761f8bd70a6607d1b22
Bstar098 <luceropintos@abc.gob.ar>
2023-10-12 12:01:18 -0300

    share button with functionality

--- components/GameReview.tsx
+++ components/GameReview.tsx
@@ -1,12 +1,17 @@
 import { getReviews } from "@/lib/reviews";
 import Image from "./Image";
 import Title from "./Title";
+import ShareLinkButton from "./ShareLinkButton";
 
 function GameReview({ title, date, image, body }) {
   return (
     <div>
       <Title>{title}</Title>
-      <p className="italic pb-2">{date}</p>
+      <div className="flex gap-2 align-middle items-baseline mb-2">
+        <p className="italic pb-2 font-bold">{date}</p>
+        <ShareLinkButton />
+      </div>
+
       <Image src={image} alt="stardew-valley-game" />
       <article
         className="prose prose-slate"
